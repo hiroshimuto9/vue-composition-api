c63cc6612d79b03d344d14761bffff72e8c6c81e
mutohiroshi <mutohiroshi@team-lab.com>
2021-04-10 18:44:52 +0900

    add: トップページの作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '@/views/home.vue'
 import Todos from '@/views/todo/todos.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
   {
     path: '/todos',
     name: 'Todos',
---
+++ src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Home</h1>
+  <div id="nav">
+    <router-link to="/todos">Todoアプリ</router-link>
+  </div>
+</template>
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
